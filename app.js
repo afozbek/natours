f820ffdde19aa780707c6bcc437c9eaec59956c4
afozbek <furkanozbek1995@gmail.com>
2019-01-09 20:30:01 +0300

    making_static_files

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const path = require('path');
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//default root directories for css,js,images(Static Files)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//view engine pug/ejs to dynamicly control front end
+app.set('view engine', 'ejs');
+app.set('views', '');
 
 //adding the route
 const route = require('./routes/route');
